14b144181a23dc46c6d0ddc128e8780c975e267d
riccardo-giuri <richicryst@gmail.com>
2023-07-24 11:53:44 +0200

    basic html structure done and milestone 1 done

---
+++ js/script.js
@@ -0,0 +1,26 @@
+Vue.createApp({
+    data() {
+        return {
+            toDoList: [{
+                id: 1,
+                text: "Ciao Sono il testo della Task",
+                done: false
+            },
+            {
+                id: 2,
+                text: "Ciao Sono il testo della Task",
+                done: false
+            },
+            {
+                id: 3,
+                text: "Ciao Sono il testo della Task",
+                done: false
+            },
+            {
+                id: 4,
+                text: "Ciao Sono il testo della Task",
+                done: false
+            },]
+        }
+    }
+}).mount("#app"); 
